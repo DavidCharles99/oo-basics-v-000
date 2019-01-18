4cd0fd420e248a41acf76a8566587fa945f5b91a
David Charles <ratlover03@gmail.com>
2019-01-18 19:53:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,11 +7,13 @@ class Book
    @title
  end
 
-def author=(author)
-  @author = author
-end
+ def author=(author)
+   @author = author
+ end
 
-def author
-  @author
-end
+ def author
+   @author
+ end
+
+
 end
